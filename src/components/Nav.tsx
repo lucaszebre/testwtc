d4043343f5b8bbbca90ebc2b53fe9626a24db69a
lucaszebre <yakirisk@gmail.com>
2023-04-17 13:27:00 +0200

    Initial commit from Create Next App

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+    import React from 'react'
+        
+        const Nav = () => {
+        return (
+            <>
+            
+            </>
+        )
+        }
+        
+        export default Nav
+        
